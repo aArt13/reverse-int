7025b77292c22c885720490196467de60e3fabc7
aArt-13 <kharytonchyk.artsiom@gmail.com>
2022-01-31 22:27:14 +0300

    init: revers-int complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let str = Math.abs(n).toString().split('');
+
+    let arr = [];
+   
+    for (let i = 0; i <= str.length-1; i++){
+   
+   arr.unshift(str[i]);
+   
+   }
+   
+    let newStr = +arr.toString().split('').join('').replace(/[\s,]/g, '');
+   
+    return newStr;
 }
